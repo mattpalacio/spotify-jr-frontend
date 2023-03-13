10e69954b4a69085ab0206a135d6d996a7230b45
Randi Blankenship <rblankenship@catalyte.io>
2023-03-13 16:47:59 -0400

    updated login page layout

--- src/app/login/feature/login.component.ts
+++ src/app/login/feature/login.component.ts
@@ -6,12 +6,28 @@ import { CommonModule } from "@angular/common";
   standalone: true,
   imports: [CommonModule],
   template: `
-  <div class="button-container">
-    <button (click)="getCode()" class="login">
-      Let's Go!
-    </button>
-  </div>
-    <div class="animation-container">
+    <div class="background-overlay"></div>
+    <div class="flex-container">
+      <div>
+        <img
+          src="../assets/36EC4E8E-3407-4013-BB62-DDF6572BB5D1-removebg-preview.png"
+          alt="Not-ify logo"
+        />
+        <h4>Reasons to listen with US</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.
+        </p>
+      </div>
+      <div class="button-container">
+        <button (click)="getCode()" class="login">Let's Groove</button>
+      </div>
+    </div>
+
       <div class="shapes">
         <div></div>
         <div></div>
@@ -24,17 +40,13 @@ import { CommonModule } from "@angular/common";
         <div></div>
         <div></div>
       </div>
-    </div>
   `,
-  styleUrls: ['./login.component.css'],
- 
+  styleUrls: ["./login.component.css"],
+
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginComponent {
-
-  ngOnInit(){
-
-  }
+  ngOnInit() {}
 
   getCode(): void {
     window.location.href = "http://127.0.0.1:8000/authorize";
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,11 +4,12 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   standalone: true,
   template: `
-    <div>
+    <div class="container">
       home page works
     </div>
   `,
   styles: [
+    '.container { background: linear-gradient(to left, #7600bc, #a000c8 ); width: 100%; height: 100vh;}'
   ]
 })
 export class HomeComponent {
