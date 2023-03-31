dc72e5ed71c7eab4c5549e1fe5e1dd4119630911
Matthew Palacio <mpalacio@catalyte.io>
2023-03-31 14:02:08 -0700

    adds web playback service to providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,17 @@ import { AppComponent } from './app.component';
 import { CookieService } from 'ngx-cookie-service';
 import { AuthGaurdService } from './auth/data-access/auth-gaurd.service';
 import { MusicService } from './home/data-access/music.service';
+import { WebPlaybackService } from './home/data-access/web-playback/web-playback.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [CookieService, AuthGaurdService, MusicService],
+  providers: [
+    CookieService,
+    AuthGaurdService,
+    MusicService,
+    WebPlaybackService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
