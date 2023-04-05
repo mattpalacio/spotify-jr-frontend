5bafbadcd52a137df7b33ebb3e3378f08440a53b
Matthew Palacio <mpalacio@catalyte.io>
2023-04-05 10:03:49 -0700

    refactors onInit guard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { exhaustMap, of } from 'rxjs';
-import { Auth } from './auth/data-access/auth.model';
-import { AuthStore } from './auth/data-access/auth.store';
+import { EMPTY, catchError, filter, of, switchMap, take, tap } from 'rxjs';
+import { AuthService } from './login/data-access/auth.service';
+import { TokenStorageService } from './login/data-access/token-storage/token-storage.service';
 
 @Component({
   selector: 'app-root',
@@ -11,25 +11,43 @@ import { AuthStore } from './auth/data-access/auth.store';
   styles: []
 })
 export class AppComponent implements OnInit {
-  constructor(private http: HttpClient, private router: Router, private auth: AuthStore) {}
+  constructor(
+    // private http: HttpClient,
+    private router: Router,
+    // private auth: AuthStore,
+    private auth: AuthService
+  ) // private tokenStorage: TokenStorageService
+  {}
 
   ngOnInit(): void {
-    const code = new URLSearchParams(window.location.search).get('code');
+    of(new URLSearchParams(window.location.search).get('code'))
+      .pipe(
+        catchError(() => EMPTY),
+        filter((code) => code !== null),
+        take(1),
+        switchMap(
+          (code) => this.auth.login$(code)
+          // .pipe(tap((auth) => this.tokenStorage.saveAuthData(auth)))
+        )
+      )
+      .subscribe(() => this.router.navigateByUrl('/home'));
 
-    if (code) {
-      const url = 'http://127.0.0.1:8000/login?code=' + code;
+    //   if (code) {
+    //     const url = 'http://127.0.0.1:8000/login?code=' + code;
 
-      this.http
-        .get<Auth>(url)
-        .pipe(
-          exhaustMap((data) => of(data)))
-        .subscribe({
-          next: (data) => {
-            this.auth.storeAuth(data);
-          },
-          error: () => this.router.navigateByUrl('/login'),
-          complete: () => this.router.navigateByUrl('/home')
-        });
-    }
+    //     this.http
+    //       .get<Auth>(url)
+    //       .pipe(exhaustMap((data) => of(data)))
+    //       .subscribe({
+    //         next: (data) => {
+    //           this.auth.storeAuth(data);
+    //         },
+    //         error: (err) => {
+    //           console.log(err);
+    //           this.router.navigateByUrl('/');
+    //         },
+    //         complete: () => this.router.navigateByUrl('/home')
+    //       });
+    //   }
   }
 }
