800c728e2f7f9b6799d1d07f480bc7e1af3dc985
Matthew Palacio <mpalacio@catalyte.io>
2023-04-05 10:11:14 -0700

    adds providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CookieService } from 'ngx-cookie-service';
-import { AuthGaurdService } from './auth/data-access/auth-gaurd.service';
+import { AuthGaurdService } from './login/data-access/auth-gaurd.service';
 import { MusicService } from './home/data-access/music.service';
 import { WebPlaybackService } from './home/data-access/web-playback/web-playback.service';
+import { JwtInterceptor } from './shared/utils/jwt.interceptor';
+import { AuthService } from './login/data-access/auth.service';
+import { TokenStorageService } from './login/data-access/token-storage/token-storage.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
     CookieService,
     AuthGaurdService,
     MusicService,
-    WebPlaybackService
+    WebPlaybackService,
+    AuthService,
+    TokenStorageService
   ],
   bootstrap: [AppComponent]
 })
